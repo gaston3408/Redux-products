ef84e5ae8379227d0dd8e711507346bf24b1beee
Gaston Camano <gaston@smartnet.com.ar>
2020-12-18 17:32:08 -0300

    redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,9 +6,9 @@ const store = createStore(
     reducer,
     compose( applyMiddleware(thunk),
 
-    typeof window === 'object' && 
-    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== undefined ?
-    window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    // typeof window === 'object' && 
+    // typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== undefined ?
+    // window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
 
 )
